a785945fa2adf12d9835e9a3c3c8390fcfa1648d
Michael Owuor <michael.owuor@gmail.com>
2022-09-30 12:05:58 +0300

    handle add to cart

--- components/navigation-header.js
+++ components/navigation-header.js
@@ -1,20 +1,36 @@
+import { useContext } from 'react';
 import Link from 'next/link';
+import { Store } from '../lib/store';
 
 const NavigationHeader = () => {
+	const { state } = useContext(Store);
+	const { cart } = state;
+
 	return (
 		<nav className="relative shadow-md">
 			<div className="container mx-auto py-4">
 				<div className="flex items-center justify-between px-4">
 					{/** logo section start */}
 					<Link href="/">
-						<a className="text-2xl font-bold">Chete Men</a>
+						<a className="text-2xl font-bold">chete men</a>
 					</Link>
 					{/** logo section end */}
 
 					{/** navigation links start */}
 					<div>
 						<Link href="/cart">
-							<a className="p-2">Cart</a>
+							<a className="p-2">
+								Cart
+								{cart.cartItems.length > 0 && (
+									<span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+										{cart.cartItems.reduce(
+											(previousValue, currentValue) =>
+												previousValue + currentValue.quantity,
+											0
+										)}
+									</span>
+								)}
+							</a>
 						</Link>
 						<Link href="/signin">
 							<a className="p-2">Sign in</a>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,12 @@
+import { useContext } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { Store } from '../../lib/store';
 import Layout from '../../components/layout';
 import data from '../../data/data';
 
 const ProductPage = () => {
+	const { state, dispatch } = useContext(Store);
 	const { query } = useRouter();
 	const { slug } = query;
 
@@ -12,6 +15,18 @@ const ProductPage = () => {
 	if (!product) {
 		return <div>The product was not found</div>;
 	}
+
+	const addToCartHandler = () => {
+		const existingItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+		const quantity = existingItem ? existingItem.quantity + 1 : 1;
+
+		if (product.numberInStock < quantity) {
+			alert('Sorry, the product is out of stock');
+			return;
+		}
+
+		dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+	};
 
 	return (
 		<Layout title={product.name}>
@@ -70,7 +85,12 @@ const ProductPage = () => {
 							<div>Status</div>
 							<div>{product.numberInStock > 0 ? 'In stock' : 'Unavailable'}</div>
 						</div>
-						<button className="primary-button w-full uppercase">Add to cart</button>
+						<button
+							className="primary-button w-full uppercase"
+							onClick={addToCartHandler}
+						>
+							Add to cart
+						</button>
 					</div>
 				</div>
 				{/* call to action end */}
