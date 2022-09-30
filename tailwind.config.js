1ba4a9eef8f3e58619d32939e6f3df43c5873ec6
Michael Owuor <michael.owuor@gmail.com>
2022-09-30 08:02:33 +0300

    create layout component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		fontFamily: {
-			poppins: 'Poppins, sans-serif',
 			ubuntu: 'Ubuntu, sans-serif',
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px',
+				'2xl': '1536px',
+			},
 		},
 		extend: {},
 	},
