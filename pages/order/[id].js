81b6cc6a73a0b9f91d5c5f62106001a1d214a4bc
Michael Owuor <michael.owuor@gmail.com>
2022-10-02 02:36:51 +0300

    pay order by PayPal

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -3,6 +3,9 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import axios from 'axios';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+import { toast } from 'react-toastify';
+import { format, parseISO } from 'date-fns';
 import Layout from '../../components/layout';
 import { getError } from '../../lib/error';
 
@@ -14,21 +17,32 @@ function reducer(state, action) {
 			return { ...state, loading: false, order: action.payload, error: '' };
 		case 'FETCH_FAIL':
 			return { ...state, loading: false, error: action.payload };
+		case 'PAY_REQUEST':
+			return { ...state, loadingPay: true };
+		case 'PAY_SUCCESS':
+			return { ...state, loadingPay: false, successPay: true };
+		case 'PAY_FAIL':
+			return { ...state, loadingPay: false, errorPay: action.payload };
+		case 'PAY_RESET':
+			return { ...state, loadingPay: false, successPay: false, errorPay: '' };
 		default:
 			state;
 	}
 }
 
 const OrderPage = () => {
-	/* order/:id */
+	const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 	const { query } = useRouter();
 	const orderId = query.id;
 
-	const [{ loading, error, order }, dispatch] = useReducer(reducer, {
-		loading: true,
-		order: {},
-		error: '',
-	});
+	const [{ loading, error, order, successPay, loadingPay }, dispatch] = useReducer(
+		reducer,
+		{
+			loading: true,
+			order: {},
+			error: '',
+		}
+	);
 
 	useEffect(() => {
 		const fetchOrder = async () => {
@@ -43,10 +57,27 @@ const OrderPage = () => {
 			}
 		};
 
-		if (!order._id || (order._id && order._id !== orderId)) {
+		if (!order._id || successPay || (order._id && order._id !== orderId)) {
 			fetchOrder();
+
+			if (successPay) {
+				dispatch({ type: 'PAY_RESET' });
+			}
+		} else {
+			const loadPaypalScript = async () => {
+				const { data: clientId } = await axios.get('/api/keys/paypal');
+
+				paypalDispatch({
+					type: 'resetOptions',
+					value: { 'client-id': clientId, currency: 'USD' },
+				});
+
+				paypalDispatch({ type: 'setLoadingStatus', value: 'pending' });
+			};
+
+			loadPaypalScript();
 		}
-	}, [order, orderId]);
+	}, [order, orderId, paypalDispatch, successPay]);
 
 	const {
 		shippingAddress,
@@ -61,6 +92,41 @@ const OrderPage = () => {
 		isDelivered,
 		deliveredOn,
 	} = order;
+
+	function createOrder(data, actions) {
+		return actions.order
+			.create({
+				purchase_units: [
+					{
+						amount: { value: totalCost },
+					},
+				],
+			})
+			.then((orderID) => {
+				return orderID;
+			});
+	}
+
+	function onApprove(data, actions) {
+		return actions.order.capture().then(async function (details) {
+			try {
+				dispatch({ type: 'PAY_REQUEST' });
+
+				const { data } = await axios.put(`/api/orders/${order._id}/pay`, details);
+
+				dispatch({ type: 'PAY_SUCCESS', payload: data });
+
+				toast.success('Order payment was successful');
+			} catch (err) {
+				dispatch({ type: 'PAY_FAIL', payload: getError(err) });
+				toast.error(getError(err));
+			}
+		});
+	}
+
+	function onError(err) {
+		toast.error(getError(err));
+	}
 
 	return (
 		<Layout title={`Order ${orderId}`}>
@@ -84,7 +150,9 @@ const OrderPage = () => {
 								{shippingAddress.country}
 							</div>
 							{isDelivered ? (
-								<div className="alert-success">Delivered on {deliveredOn}</div>
+								<div className="alert-success">
+									Delivered on {format(parseISO(deliveredOn), 'MMMM do, yyyy')}
+								</div>
 							) : (
 								<div className="alert-error">Not delivered</div>
 							)}
@@ -96,7 +164,9 @@ const OrderPage = () => {
 							<h2 className="mb-2 text-lg">Payment Method</h2>
 							<div>{paymentMethod}</div>
 							{isPaid ? (
-								<div className="alert-success">Paid on {paidOn}</div>
+								<div className="alert-success">
+									Paid on {format(parseISO(paidOn), 'MMMM do, yyyy')}
+								</div>
 							) : (
 								<div className="alert-error">Not paid</div>
 							)}
@@ -175,6 +245,22 @@ const OrderPage = () => {
 										<div>${totalCost}</div>
 									</div>
 								</li>
+								{!isPaid && (
+									<li>
+										{isPending ? (
+											<div>Loading...</div>
+										) : (
+											<div className="w-full rounded">
+												<PayPalButtons
+													createOrder={createOrder}
+													onApprove={onApprove}
+													onError={onError}
+												></PayPalButtons>
+											</div>
+										)}
+										{loadingPay && <div>Loading...</div>}
+									</li>
+								)}
 							</ul>
 						</div>
 						{/* call to action end */}
