7e2ff46bf2491c3cdf455d597ad00fdefe9b3a1a
Michael Owuor <michael.owuor@gmail.com>
2022-10-03 00:45:54 +0300

    create and delete products

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -1,6 +1,8 @@
 import { useEffect, useReducer } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import Layout from '../../components/layout';
 import { getError } from '../../lib/error';
 
@@ -12,17 +14,57 @@ function reducer(state, action) {
 			return { ...state, loading: false, products: action.payload, error: '' };
 		case 'FETCH_FAIL':
 			return { ...state, loading: false, error: action.payload };
+		case 'CREATE_REQUEST':
+			return { ...state, loadingCreate: true };
+		case 'CREATE_SUCCESS':
+			return { ...state, loadingCreate: false };
+		case 'CREATE_FAIL':
+			return { ...state, loadingCreate: false };
+		case 'DELETE_REQUEST':
+			return { ...state, loadingDelete: true };
+		case 'DELETE_SUCCESS':
+			return { ...state, loadingDelete: false, successDelete: true };
+		case 'DELETE_FAIL':
+			return { ...state, loadingDelete: false };
+		case 'DELETE_RESET':
+			return { ...state, loadingDelete: false, successDelete: false };
 		default:
 			state;
 	}
 }
 
 const AdminProductsPage = () => {
-	const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+	const [
+		{ loading, error, products, loadingCreate, successDelete, loadingDelete },
+		dispatch,
+	] = useReducer(reducer, {
 		loading: true,
 		products: [],
 		error: '',
 	});
+
+	const router = useRouter();
+
+	const createHandler = async () => {
+		if (!window.confirm('Are you sure?')) {
+			return;
+		}
+
+		try {
+			dispatch({ type: 'CREATE_REQUEST' });
+
+			const { data } = await axios.post(`/api/admin/products`);
+
+			dispatch({ type: 'CREATE_SUCCESS' });
+
+			toast.success('Product created successfully');
+
+			router.push(`/admin/product/${data.product._id}`);
+		} catch (err) {
+			dispatch({ type: 'CREATE_FAIL' });
+			toast.error(getError(err));
+		}
+	};
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -37,8 +79,31 @@ const AdminProductsPage = () => {
 			}
 		};
 
-		fetchData();
-	}, []);
+		if (successDelete) {
+			dispatch({ type: 'DELETE_RESET' });
+		} else {
+			fetchData();
+		}
+	}, [successDelete]);
+
+	const deleteHandler = async (productId) => {
+		if (!window.confirm('Are you sure?')) {
+			return;
+		}
+
+		try {
+			dispatch({ type: 'DELETE_REQUEST' });
+
+			await axios.delete(`/api/admin/products/${productId}`);
+
+			dispatch({ type: 'DELETE_SUCCESS' });
+
+			toast.success('Product deleted successfully');
+		} catch (err) {
+			dispatch({ type: 'DELETE_FAIL' });
+			toast.error(getError(err));
+		}
+	};
 
 	return (
 		<Layout title="Administrative Dashboard Products">
@@ -69,7 +134,19 @@ const AdminProductsPage = () => {
 				</div>
 
 				<div className="overflow-x-auto md:col-span-3">
-					<h1 className="mb-5 text-xl">Products</h1>
+					<div className="flex justify-between">
+						<h1 className="mb-5 text-xl">Products</h1>
+
+						{loadingDelete && <div>Deleting item...</div>}
+
+						<button
+							disabled={loadingCreate}
+							onClick={createHandler}
+							className="primary-button rounded"
+						>
+							{loadingCreate ? 'Loading' : 'Create'}
+						</button>
+					</div>
 
 					{loading ? (
 						<div>Loading...</div>
@@ -105,7 +182,11 @@ const AdminProductsPage = () => {
 														Edit
 													</a>
 												</Link>
-												<button type="button" className="default-button m-1 uppercase">
+												<button
+													type="button"
+													className="default-button m-1 uppercase"
+													onClick={() => deleteHandler(product._id)}
+												>
 													Delete
 												</button>
 											</td>
